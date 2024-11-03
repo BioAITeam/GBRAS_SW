40c4c6444b678c24c15573b979f95c98df558cc7
arteagabrayan <arteagabrayan99ah@gmail.com>
2024-11-03 13:45:27 -0500

    Updating library versions and adding a requirements file and venv mode

--- GBRAS_SW.py
+++ GBRAS_SW.py
@@ -1,63 +1,90 @@
-#GBRAS_SW
-#Libraries
+# GBRAS_SW
+# Libraries
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 0 = INFO, 1 = WARNING, 2 = ERROR, 3 = FATAL
+
 import tensorflow as tf
 import numpy as np
 import cv2
 import argparse
 import glob
 import xlsxwriter
-import os
+import warnings
 import datetime
 
-#3xTanH ACTIVATION FUNCTION
+warnings.filterwarnings("ignore")
+tf.get_logger().setLevel('ERROR')
+
+# 3xTanH ACTIVATION FUNCTION
 def Tanh3(x):
-    tanh3 = tf.keras.activations.tanh(x)*3
+    tanh3 = tf.keras.activations.tanh(x) * 3
     return tanh3
 
-#Load an image
+# Load an image
 def load_image(path_image):
-	I = cv2.imread(path_image, cv2.IMREAD_GRAYSCALE)
-	I = np.array(I)
-	return I
+    try:
+        I = cv2.imread(path_image, cv2.IMREAD_GRAYSCALE)
+        if I is None:
+            raise ValueError("Image not found or unable to load.")
+        I = np.array(I)
+        return I
+    except Exception as e:
+        print(f"Error loading image {path_image}: {e}")
+        return None
 
-#Prediction if an image is cover or stego
+# Prediction if an image is cover or stego
 def cover_or_stego(model, image):
-    image = np.reshape(image,(1,256,256,1))
-    acc_val = model.predict(image)
-    prediction = np.round(acc_val)
-    if (prediction == np.array([1, 0])).all():
-        worksheet.write(row, 1, "cover")
-        worksheet.write(row, 2, float("{:.1f}".format(acc_val[0][0]*100))) 
-        print("Prediction: COVER, accuracy: {:.1f}%\n".format(acc_val[0][0]*100))
-    else:
-        worksheet.write(row, 1, "stego")
-        worksheet.write(row, 2, float("{:.1f}".format(acc_val[0][1]*100))) 
-        print("Prediction: STEGO, accuracy: {:.1f}%\n".format(acc_val[0][1]*100))
+    try:
+        image = np.reshape(image, (1, 256, 256, 1))
+        acc_val = model.predict(image)
+        prediction = np.round(acc_val)
+        if (prediction == np.array([1, 0])).all():
+            worksheet.write(row, 1, "cover")
+            worksheet.write(row, 2, float("{:.1f}".format(acc_val[0][0] * 100)))
+            print("Prediction: COVER, accuracy: {:.1f}%".format(acc_val[0][0] * 100))
+        else:
+            worksheet.write(row, 1, "stego")
+            worksheet.write(row, 2, float("{:.1f}".format(acc_val[0][1] * 100)))
+            print("Prediction: STEGO, accuracy: {:.1f}%".format(acc_val[0][1] * 100))
+    except Exception as e:
+        print(f"Error during prediction: {e}")
+        worksheet.write(row, 1, "Error")
+        worksheet.write(row, 2, "N/A")
 
-#Load a model
-##Option models ./models/
-###S-UNIWARD_0.4bpp.hdf5, S-UNIWARD_0.2bpp.hdf5, WOW_0.4bpp.hdf5, WOW_0.2bpp.hdf5
+# Load a model
+# Option models ./models/
+# S-UNIWARD_0.4bpp.hdf5, S-UNIWARD_0.2bpp.hdf5, WOW_0.4bpp.hdf5, WOW_0.2bpp.hdf5
 def load_model(path_model):
     global model_name
     model_name = os.path.basename(path_model)
     if model_name.endswith('.hdf5'):
         model_name = model_name[:-5]
-    
-    model = tf.keras.models.load_model(path_model, custom_objects={'Tanh3':Tanh3})
-    return model
+    try:
+        model = tf.keras.models.load_model(path_model, custom_objects={'Tanh3': Tanh3})
+        return model
+    except Exception as e:
+        print(f"Error loading model {path_model}: {e}")
+        return None
 
 def write_exel_file():
-    global worksheet,workbook,row
+    global worksheet, workbook, row
     path_log = './logs'
-    if not os.path.exists(path_log):
-        os.makedirs(path_log)
-    workbook = xlsxwriter.Workbook(path_log+'/GBRAS-Net_'+str(datetime.datetime.now().isoformat()[:19].replace("T", "_").replace(":","-"))+"_"+model_name+".xlsx") 
-    worksheet = workbook.add_worksheet()
-    cell_format = workbook.add_format({'bold': True, 'font_color': 'red'})
-    worksheet.write(0, 0, "image", cell_format) 
-    worksheet.write(0, 1, "prediction", cell_format) 
-    worksheet.write(0, 2, "accuracy [%]", cell_format) 
-    row = 1
+    try:
+        if not os.path.exists(path_log):
+            os.makedirs(path_log)
+        workbook = xlsxwriter.Workbook(
+            path_log + '/GBRAS-Net_' + str(datetime.datetime.now().isoformat()[:19].replace("T", "_").replace(":", "-")) + "_" + model_name + ".xlsx")
+        worksheet = workbook.add_worksheet()
+        cell_format = workbook.add_format({'bold': True, 'font_color': 'red'})
+        worksheet.write(0, 0, "image", cell_format)
+        worksheet.write(0, 1, "prediction", cell_format)
+        worksheet.write(0, 2, "accuracy [%]", cell_format)
+        row = 1
+    except Exception as e:
+        print(f"Error setting up Excel file: {e}")
+        workbook = None
+        worksheet = None
+        row = 1
 
 if __name__ == '__main__':
     print("\n############################################################")
@@ -72,22 +99,30 @@ if __name__ == '__main__':
     print("#                    WOW_0.2bpp.hdf5                       #")
     print("############################################################\n")
 
-    ### read parameters
+    # Read parameters
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--path-images', required=True, dest='path_image',help='Path of the images to be classified')
-    parser.add_argument('-m', '--path-model', required=True, dest='path_model',help='Path of the model')
+    parser.add_argument('-i', '--path-images', required=True, dest='path_image', help='Path of the images to be classified')
+    parser.add_argument('-m', '--path-model', required=True, dest='path_model', help='Path of the model')
     parser.add_argument('-v', '--version', action='version', version='%(prog)s v1.0')
 
     options = parser.parse_args()
     path_image = options.path_image
     path_model = options.path_model
     Loaded_model = load_model(path_model)
+    if Loaded_model is None:
+        print("Failed to load model. Exiting.")
+        exit(1)
     write_exel_file()
+    if workbook is None or worksheet is None:
+        print("Failed to set up Excel file. Exiting.")
+        exit(1)
 
-    # to load all images in the path
-    for image in glob.glob(path_image+"/*.pgm"):
+    # Load all images in the path
+    for image in glob.glob(path_image + "/*.pgm"):
         Loaded_image = load_image(image)
-        cover_or_stego(Loaded_model, Loaded_image) #Prediction
-        worksheet.write(row, 0, os.path.basename(image)) 
+        if Loaded_image is None:
+            continue  # Skip this image
+        cover_or_stego(Loaded_model, Loaded_image)  # Prediction
+        worksheet.write(row, 0, os.path.basename(image))
         row += 1
-    workbook.close() 
+    workbook.close()
--- README.md
+++ README.md
@@ -1,56 +1,71 @@
 # GBRAS_SW: a software for steganalysis in the spatial domain.
 GBRAS_SW is a software for the detection of steganographic images in the spatial domain. It is based in the GBRAS architecture that is in-depth explanate in [1]. GBRAS_SW shows high precision rates for the prediction of steganographic images. This software maintains, for preprocessing stage, the 30 SRM filters and has a 3xTanH activation function. Also, it uses the ELU activation function in all feature extraction convolutions, and shortcuts for feature extraction and separable and depthwise convolutions. This software does not use fully connected layers; the network uses a softmax directly after global average pooling.
+
 ## Prerequisites
 The GBRAS_SW requires the following libraries and frameworks.
 
-- TensorFlow 
--	numPy 
-- OpenCV 
-- argparse
-- glob
-- XlsxWriter
-- os
-- datetime
-
-
-GBRAS_SW  was developed in the Python3 (3.8) programming language.
+- GIT
+- Python 3.10 (64-bit)
+- Windows 11
+- VSC (Visual Studio Code)
 
 ## Installation:
-We highly recommend to use and install Python packages within an Anaconda enviroment. To create, execute the command below:
+
+Create a folder, and inside you need to clone the repository
+
 ```
-conda create --name GBRAS_SW python=3.8
+git clone https://github.com/BioAITeam/GBRAS_SW.git . 
 ```
-So, activate it
+
+In terminal create a venv using Python 3.10
+
 ```
-conda activate GBRAS_SW 
+py -3.10 -m venv venv
 ```
-installed the framework
+
+Activate it
+
 ```
-conda install -c anaconda keras-gpu==2.4.3
+venv\scripts\activate
 ```
-Now, install the libraries.
+
 ```
-pip install opencv-python
-pip install scikit-image
-conda install -c conda-forge argparse
-conda install -c conda-forge xlsxwriter
-conda install -c jmcmurray os
-conda install -c trentonoliphant datetime
+pip install -r requirements.txt
 ```
+
+The requirements file will install:
+
+- https://files.pythonhosted.org/packages/02/95/87f6583564e7095d4959ed42f4d6d79c65bfcdbdd9675b5ce2a0d06ac79c/tensorflow_gpu-2.8.0-cp310-cp310-win_amd64.whl
+- opencv-python==4.10.0.84
+- scikit-image==0.24.0
+- xlsxwriter==3.2.0
+- protobuf==3.20.3
+- numpy==1.23.5
+
+In the case of TensorFlow 2.8.0, you can see the version in: https://pypi.org/project/tensorflow-gpu/2.8.0/#files
+
+This corresponds to tensorflow_gpu-2.8.0-cp310-cp310-win_amd64.whl (438.0 MB), you need to have Python 3.10
+
 ## GBRAS_SW execution
-After installing all the prerequisites, you must clone the repository of the current version of GBRAS_SW using.
-```
-git clone https://github.com/BioAITeam/GBRAS_SW.git
-```
-Then you might run as following:
+In the repository, there are two folders, one with images and the other with models. The images folder contains eighty cover and stego images for testing the software. You can add more images to the folder to test the software's accuracy in detecting cover and stego image in the spatial domain. The format of the images is Portable Gray Map (PGM). In the model folder, there are four models S_UNIWARD and WOW, with two payloads, 0.4 and 0.2 bpp, respectively. You can choose any of the four models to perform a cover or stego image prediction, for example:
+
+Run as following:
 ```
 python GBRAS_SW.py -i ./images -m ./models/S-UNIWARD_0.4bpp.hdf5
 ```
-In the repository, there are two folders, one with images and the other with models. The images folder contains eighty cover and stego images for testing the software. You can add more images to the folder to test the software's accuracy in detecting cover and stego image in the spatial domain.  The format of the images is Portable Gray Map (PGM). In the model folder, there are four models S_UNIWARD and WOW, with two payloads, 0.4 and 0.2 bpp, respectively. You can choose any of the four models to perform a cover or stego image prediction, for example:
 
 ```
 python GBRAS_SW.py -i ./images -m ./models/WOW_0.4bpp.hdf5
 ```
+
+```
+python GBRAS_SW.py -i ./images -m ./models/S-UNIWARD_0.2bpp.hdf5
+```
+
+```
+python GBRAS_SW.py -i ./images -m ./models/WOW_0.2bpp.hdf5
+```
+
 ## Authors
 Universidad Autónoma de Manizales (https://www.autonoma.edu.co/)
 
